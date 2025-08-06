f4434f22c220c256532fd0d9a9e70979c8f91854
Shobhit Nair <shobhit.nair@qburst.com>
2025-08-06 11:51:40 +0530

    Initialized the vite app, added tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='font-bold text-3xl text-center mt-10 text-green-500'>
+      Hello everyone. Lets build Netflix-GPT
+    </div>
+  )
+}
+
+export default App
